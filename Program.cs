92d2f63ad171a85f79e3554d098215cd4bbf2cdf
Galboss <68510218+Galboss@users.noreply.github.com>
2024-01-10 15:48:15 -0600

    Template Movie Portfolio Blazor WASM
    Initial template for Blazor Movie Portforlio on WASM

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Portfolio2;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+builder.Services.AddBlazoredSessionStorageAsSingleton();
+await builder.Build().RunAsync();
